027f2a9e4a9e7d21657103140468bcc4a6f014a4
Felipe da Silva <felipe.silva@corebiz.com.br>
2017-11-22 14:48:07 -0200

    ajuste js

--- challenge/script.js
+++ challenge/script.js
@@ -1,42 +1,4 @@
-'use strict';
-
-var $template =  '<div class="countrie" v-for="item in countrie">' +
-                '</div>';
-
-// registro
-Vue.component('countrie', {
-    template: $template,
-    data: function () {
-        return {
-            countrie: []
-        }
-    },
-    created() {
-        this.getCountrie();
-    },
-    methods: {
-        getCountrie(){
-            var _this = this;
-            $.ajax({
-                url: 'https://restcountries.eu/rest/v2/',
-                method: 'GET'
-            }).done(function (response) {
-                console.log(response);
-                _this._Countrie(response);
-            });
-        },
-        _Countrie(data){
-            this.countrie = data;
-        }
-    }
-});
-
-// cria a instância raiz
-new Vue({
-    el: '._countrie'
-});
-
-/*;(function($, window, document, undefined) {
+;(function($, window, document, undefined) {
 
 	$.ajax({
 		url: 'https://restcountries.eu/rest/v2/',
@@ -45,4 +7,4 @@ new Vue({
 		console.log(data);
 	});
 
-})(jQuery, window, document);*/
+})(jQuery, window, document);
