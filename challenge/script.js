857156a62a672ef0fd3bd1bd75e653b65ab97727
Felipe da Silva <felipe.silva@corebiz.com.br>
2017-11-22 14:39:43 -0200

    Rendering api in vue.js

--- challenge/script.js
+++ challenge/script.js
@@ -1,6 +1,42 @@
 'use strict';
 
-;(function($, window, document, undefined) {
+var $template =  '<div class="countrie" v-for="item in countrie">' +
+                '</div>';
+
+// registro
+Vue.component('countrie', {
+    template: $template,
+    data: function () {
+        return {
+            countrie: []
+        }
+    },
+    created() {
+        this.getCountrie();
+    },
+    methods: {
+        getCountrie(){
+            var _this = this;
+            $.ajax({
+                url: 'https://restcountries.eu/rest/v2/',
+                method: 'GET'
+            }).done(function (response) {
+                console.log(response);
+                _this._Countrie(response);
+            });
+        },
+        _Countrie(data){
+            this.countrie = data;
+        }
+    }
+});
+
+// cria a instância raiz
+new Vue({
+    el: '._countrie'
+});
+
+/*;(function($, window, document, undefined) {
 
 	$.ajax({
 		url: 'https://restcountries.eu/rest/v2/',
@@ -9,4 +45,4 @@
 		console.log(data);
 	});
 
-})(jQuery, window, document);
+})(jQuery, window, document);*/
