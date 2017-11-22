d6139a8bdc9cb43fb325f111e90afa2d88b2ff33
Felipe da Silva <felipe.silva@corebiz.com.br>
2017-11-22 14:57:21 -0200

    error rendering vue.js

--- challenge/script.js
+++ challenge/script.js
@@ -1,4 +1,41 @@
-;(function($, window, document, undefined) {
+var $product =  '<div class="product" v-for="item in countrie">' +
+                    '<h3> {{ item.name }} </h3>' +
+                '</div>';
+
+// registro
+Vue.component('countrie', {
+    template: $product,
+    data: function () {
+        return {
+            countrie: []
+        }
+    },
+    created() {
+        this.getCountrie();
+    },
+    methods: {
+        getCountrie(){
+            var _this = this;
+            $.ajax({
+                url: "https://restcountries.eu/rest/v2/",
+                dataType: 'json'
+            }).done(function (response) {
+                console.log(response);
+                _this.makeCountrie(response);
+            });
+        },
+        makeCountrie(data){
+            this.countrie = data;
+        }
+    }
+});
+
+// cria a instância raiz
+new Vue({
+    el: '._countrie'
+});
+
+/*;(function($, window, document, undefined) {
 
 	$.ajax({
 		url: 'https://restcountries.eu/rest/v2/',
@@ -7,4 +44,4 @@
 		console.log(data);
 	});
 
-})(jQuery, window, document);
+})(jQuery, window, document);*/
