4e68ad258271df19befb974e7d1ad91c1c4781ce
Felipe da Silva <silvacontatoweb@gmail.com>
2018-01-03 16:36:14 -0200

    fix: remove console

--- projects/makeup/assets/js/alcon_0.2.1.compiled.js
+++ projects/makeup/assets/js/alcon_0.2.1.compiled.js
@@ -7829,7 +7829,7 @@ Router events
           return oProduct.pl === oProductLine.key;
         });
         if (!oProduct) {
-          return console.log(oProductLine);
+          // return console.log(oProductLine); CONSOLE BACANINHA
         } else {
           oProductLine.category = Alcon.Utils.Helpers.fnGetCategoryByFeature(oProduct.feature);
           return oProductLine.feature = oProduct.feature;
